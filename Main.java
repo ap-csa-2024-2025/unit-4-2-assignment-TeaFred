b003e309a3752dbc800c32593843dff05b8282fe
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-11-14 19:22:48 +0000

    saved?

--- Main.java
+++ Main.java
@@ -4,6 +4,22 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    int lowerB;
+    int upperB;
+
+    System.out.println("Please enter two integers: ");
+    lowerB = sc.nextInt();
+    upperB = sc.nextInt();
+    int theNumNCount = lowerB;
+
+    while ((theNumNCount >= lowerB)  && (theNumNCount <=upperB))
+    {
+      if (theNumNCount % 2 == 1)
+      {
+        System.out.println(theNumNCount);
+      }
+      theNumNCount++;
+    }
   }
 }
